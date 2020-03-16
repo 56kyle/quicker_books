3c587b8179a51e0b0b60bc106302e151c0424c31
56kyle <oliverke@mail.uc.edu>
2020-03-16 17:04:59 -0400

    bit better now

--- lib/quicker_books.rb
+++ lib/quicker_books.rb
@@ -11,18 +11,19 @@ module QuickerBooks
     puts self.source_paths
     source_root File.expand_path('../quicker_books/templates', __FILE__)
 
-    attr_accessor :part_children, :part_parent, :part_parent_name, :part_path, :part, :part_name
+    attr_accessor :part_children, :part, :part_name
 
-    def make_directories_from(input= 'QBXML')
+    def make_directories_from(input= 'QBXMLMsgsRq')
       @q = Qbxml.new(:qb, '13.0')
       @tabs = 0
       starting_qbxml = @q.describe(input)
-      starting_qbxml.traverse do |part|
-        make_dir(part) unless part.name.to_s.include?('()') || part.name.to_s == 'text' || part.name.to_s == 'comment'
+      starting_qbxml.children.each do |c|
+        make_dir(c) unless c.name.to_s.include?('()') || c.name.to_s == 'text' || c.name.to_s == 'comment'
       end
     end
 
     no_commands do
+
       def tabs
         result = ""
         @tabs.times do
@@ -30,6 +31,7 @@ module QuickerBooks
         end
         result
       end
+
       def a_class(a_class_name= nil, super_class_name= nil)
         if a_class_name && block_given?
           line "class #{a_class_name}#{super_class_name ? ' < ' + super_class_name : nil}"
@@ -39,6 +41,7 @@ module QuickerBooks
           line 'end'
         end
       end
+
       def a_method(a_method_name= nil, parameters= nil)
         if parameters.is_a?(Hash)
           parameters = parameters.map{|key, val| "#{key.to_s} = #{val}" }
@@ -51,19 +54,15 @@ module QuickerBooks
           line 'end'
         end
       end
+
       def a_file
         @lines = []
         if block_given?
-          part_path_adjusted = @part_path[0, @part_path.size - 1]
-          part_path_adjusted.each do |a_parent|
-            line "module #{a_parent.camelize}Module"
-            @tabs+=1
-          end
+          line "module QuickerBooks"
+          @tabs+=1
           yield
-          part_path_adjusted.each do
-            @tabs-=1
-            line "end"
-          end
+          @tabs-=1
+          line "end"
         end
         @lines
       end
@@ -74,85 +73,28 @@ module QuickerBooks
         @lines
       end
 
-      def rq
-        a_file do
-          base_entity do
-            a_method 'requests', %w(job session data) do
-              requests_content
-            end
-          end
-        end
-      end
-
-      def rs
-        a_file do
-          base_entity do
-            a_method 'handle_response', %w(response session job requests data) do
-              handle_response_content
-            end
-          end
-        end
-      end
-
       def struct_base
         a_file do
-          base_entity do
+          a_class @part_name, 'QBWC::Worker' do
+            a_method 'initialize' do
+              line 'nil'
+            end
             a_method 'requests', %w(job session data) do
               requests_content
             end
             a_method 'handle_response', %w(response session job requests data) do
-              handle_response_content
+              line "nil"
             end
           end
         end
-      end
-
-
-      def base_entity
-        a_method @part_name.underscore do
-          line "#{@part_parent_name}Module::#{@part_name}.new"
-        end
-        a_class @part_name, @part_parent_name do
-          line "attr_accessor :required"
-          a_method 'initialize' do
-            initializer_content
-          end
-          yield if block_given?
-        end
-      end
-
-      def initializer_content
-        line "@required = #{@required.to_s}"
-        line "self.class.include #{@part_name}Module unless #{@part_name}.include?(#{@part_name}Module)" if @part_children.present?
       end
 
       def requests_content
-        if @part_children && @part_children.present?
-          line '{'
-          @tabs+=2
-          line ":#{@part_name.underscore} => {"
-          @tabs+=1
-          the_last_name = @part_children.last.name.camelize
-          @part_children.map{|c| c.name.camelize }.each do |c_name|
-            line ":#{c_name} => #{c_name.underscore}.requests(job, session, data)#{',' unless the_last_name == c_name}"
-          end
-          @tabs-=1
-          line '}'
-          @tabs-=2
-          line '}'
+        new_hash = Hash.from_xml(@part.to_xml).deep_transform_keys do |key|
+          key.to_s.underscore
         end
-      end
-
-      def handle_response_content
-        line "block_given? ? super{yield['#{@part.name}']} : super{response['#{@part.name}']}"
-      end
-
-      def find_required_parts
-        @required = []
-        @part.children.each do |c|
-          c.content.scan(/(?:BEGIN OR:.*have )(.*)/).map do |one_of_this_array|
-            @required << one_of_this_array[0].gsub('OR', '||') if one_of_this_array.present?
-          end
+        new_hash.pretty_inspect.gsub("=>{", "=>\n{").gsub(", ", ",\n").split("\n").each do |hash_line|
+          line hash_line
         end
       end
 
@@ -160,22 +102,11 @@ module QuickerBooks
         part.children.select{|c|c.name != 'text' && c.name != 'comment'}
       end
 
-      def module_exists?(name, base = self.class)
-        base.const_defined?(name) &&
-            base.const_get(name).instance_of?(::Module)
-      end
-
       def make_dir(instance_start)
-        part = instance_start
-        @part = part
-        @part_name = part.name.camelize
-        @part_path = part.path&.split('/')[1,100]
-        @part_parent = part.parent
-        @part_parent_name = @part_parent&.name.camelize
+        @part = instance_start
+        @part_name = @part.name.camelize
         @part_children = find_children(part)
-        @required = []
-        #empty_directory('./app/'+@part_path.join('Module/').underscore) if @part_children
-        template('./struct.erb', './app/'+@part_path.join('Module/').underscore+'.rb')
+        template('./struct.erb', './lib/'+@part_name.underscore+'.rb')
       end
     end
   end
